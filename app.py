ce91e4309cd6510f61e44df3bf502b7a0598fa1d
futomani <bushi.dou813@gmail.com>
2025-02-26 17:31:43 +0900

    Update app.py

--- app.py
+++ app.py
@@ -47,3 +47,8 @@ def reply(reply_token, text):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "LINE Bot is running!", 200
+
