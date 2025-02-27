47e50e46584dc6818ddb3cf2ab3327f2802e4752
futomani <bushi.dou813@gmail.com>
2025-02-27 12:11:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -75,5 +75,8 @@ def home():
     """ 確認用のルート """
     return "LINE Bot is running!", 200
 
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # PORT環境変数を取得（デフォルト5000）
+    app.run(host="0.0.0.0", port=port)
