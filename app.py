d242487a6cfb76acdbedb758cf4175a6892c414b
futomani <bushi.dou813@gmail.com>
2025-02-27 12:26:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -74,9 +74,3 @@ def callback():
 def home():
     """ 確認用のルート """
     return "LINE Bot is running!", 200
-
-import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # PORT環境変数を取得（デフォルト5000）
-    app.run(host="0.0.0.0", port=port)
